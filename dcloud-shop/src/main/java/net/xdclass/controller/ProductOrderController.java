8d1d2de1c87db1835c538ce0795aa88921294735
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-11-28 01:25:45 +0800

    feat: 订单代码相关类生成

---
+++ dcloud-shop/src/main/java/net/xdclass/controller/ProductOrderController.java
@@ -0,0 +1,15 @@
+package net.xdclass.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/28 01:13
+ */
+@RestController
+@RequestMapping("/productOrderDO")
+public class ProductOrderController {
+
+}
---
+++ dcloud-shop/src/main/java/net/xdclass/mapper/ProductOrderMapper.java
@@ -0,0 +1,13 @@
+package net.xdclass.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.xdclass.model.ProductOrderDO;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/28 01:24
+ */
+public interface ProductOrderMapper extends BaseMapper<ProductOrderDO> {
+
+}
