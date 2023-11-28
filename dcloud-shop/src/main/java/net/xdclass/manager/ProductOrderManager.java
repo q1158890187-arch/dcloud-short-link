450671481b4a7863fa2238ef0c969a4f2c1bf321
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-11-29 00:19:14 +0800

    feat: manager层

---
+++ dcloud-shop/src/main/java/net/xdclass/manager/ProductOrderManager.java
@@ -0,0 +1,53 @@
+package net.xdclass.manager;
+
+import net.xdclass.model.ProductOrderDO;
+
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/28 13:07
+ */
+public interface ProductOrderManager {
+
+
+    /***
+     * 新增
+     * @param productOrderDO
+     * @return
+     */
+    int add(ProductOrderDO productOrderDO);
+
+
+    /**
+     * 通过订单号和账号查询
+     * @param outTradeNo
+     * @param accountNo
+     * @return
+     */
+    ProductOrderDO findByOutTradeNoAndAccountNo(String outTradeNo,Long accountNo);
+
+
+    /**
+     * 更新订单状态
+     * @param outTradeNo
+     * @param accountNo
+     * @param newState
+     * @param oldState
+     * @return
+     */
+    int updateOrderPayState(String outTradeNo,Long accountNo,String newState,String oldState);
+
+
+    /**
+     * 分页查看订单列表
+     * @param page
+     * @param size
+     * @param accountNo
+     * @param state
+     * @return
+     */
+    Map<String,Object> page(int page, int size, Long accountNo, String state);
+
+}
---
+++ dcloud-shop/src/main/java/net/xdclass/mapper/ProductOrderManagerImpl.java
@@ -0,0 +1,90 @@
+package net.xdclass.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import net.xdclass.manager.ProductOrderManager;
+import net.xdclass.model.ProductOrderDO;
+import net.xdclass.vo.ProductOrderVO;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/28 13:08
+ */
+@Component
+public class ProductOrderManagerImpl implements ProductOrderManager {
+
+    @Resource
+    private ProductOrderMapper productOrderMapper;
+
+    @Override
+    public int add(ProductOrderDO productOrderDO) {
+        return productOrderMapper.insert(productOrderDO);
+    }
+
+    @Override
+    public ProductOrderDO findByOutTradeNoAndAccountNo(String outTradeNo, Long accountNo) {
+
+        ProductOrderDO productOrderDO = productOrderMapper.selectOne(new LambdaQueryWrapper<ProductOrderDO>()
+                .eq(ProductOrderDO::getOutTradeNo, outTradeNo)
+                .eq(ProductOrderDO::getAccountNo, accountNo));
+        return productOrderDO;
+    }
+
+    @Override
+    public int updateOrderPayState(String outTradeNo, Long accountNo, String newState, String oldState) {
+
+        int rows = productOrderMapper.update(null, new LambdaUpdateWrapper<ProductOrderDO>()
+                .eq(ProductOrderDO::getOutTradeNo, outTradeNo)
+                .eq(ProductOrderDO::getAccountNo, accountNo)
+                .eq(ProductOrderDO::getState, oldState)
+                .set(ProductOrderDO::getState, newState));
+        return rows;
+    }
+
+    @Override
+    public Map<String, Object> page(int page, int size, Long accountNo, String state) {
+
+        Page<ProductOrderDO> pageInfo = new Page<>(page, size);
+
+        IPage<ProductOrderDO> orderDOIPage;
+
+        if (StringUtils.isBlank(state)) {
+
+            orderDOIPage = productOrderMapper.selectPage(pageInfo, new QueryWrapper<ProductOrderDO>().eq("account_no", accountNo));
+
+        } else {
+
+            orderDOIPage = productOrderMapper.selectPage(pageInfo, new QueryWrapper<ProductOrderDO>()
+                    .eq("account_no", accountNo)
+                    .eq("state", state));
+        }
+
+        List<ProductOrderDO> orderDOIPageRecords = orderDOIPage.getRecords();
+
+        List<ProductOrderVO> productOrderVOList = orderDOIPageRecords.stream().map(obj -> {
+            ProductOrderVO productOrderVO = new ProductOrderVO();
+            BeanUtils.copyProperties(obj, productOrderVO);
+            return productOrderVO;
+        }).collect(Collectors.toList());
+
+        Map<String,Object> pageMap = new HashMap<>(3);
+        pageMap.put("total_record",orderDOIPage.getTotal());
+        pageMap.put("total_page",orderDOIPage.getPages());
+        pageMap.put("current_data",productOrderVOList);
+
+        return pageMap;
+    }
+}
