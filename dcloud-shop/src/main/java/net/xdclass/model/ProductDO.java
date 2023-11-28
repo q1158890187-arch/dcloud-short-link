1c085c78510958665403118aa33b04587fdd9460
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-11-28 13:05:39 +0800

    feat: 订单分表配置

--- dcloud-shop/src/main/java/net/xdclass/model/ProductDO.java
+++ dcloud-shop/src/main/java/net/xdclass/model/ProductDO.java
@@ -22,7 +22,6 @@ public class ProductDO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
