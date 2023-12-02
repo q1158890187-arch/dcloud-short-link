28a75f40c66541fb1144c66a9f4dda3ef11042e9
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-12-02 20:42:53 +0800

    feat: 自定义注解

---
+++ dcloud-common/src/main/java/net/xdclass/annotation/RepeatSubmit.java
@@ -0,0 +1,35 @@
+package net.xdclass.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义防重提交
+ *
+ * @author zhengqinghua
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RepeatSubmit {
+
+
+    /**
+     * 防重提交，支持两种，一个是方法参数，一个是令牌
+     */
+    enum Type { PARAM, TOKEN }
+
+    /**
+     * 默认防重提交，是方法参数
+     * @return
+     */
+    Type limitType() default Type.PARAM;
+
+
+    /**
+     * 加锁过期时间，默认是5秒
+     * @return
+     */
+    long lockTime() default 5;
+
+}
+
