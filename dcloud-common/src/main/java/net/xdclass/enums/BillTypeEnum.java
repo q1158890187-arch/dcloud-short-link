6cc13104098d973be1688045318049463e76845c
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-12-02 19:28:31 +0800

    feat: 下单流程

---
+++ dcloud-common/src/main/java/net/xdclass/enums/BillTypeEnum.java
@@ -0,0 +1,23 @@
+package net.xdclass.enums;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/30 00:28
+ */
+public enum BillTypeEnum {
+    /**
+     * 不用发票
+     */
+    NO_BILL,
+
+    /**
+     * 纸质发票
+     */
+    PAPER_BILL,
+
+    /**
+     * 电子发票
+     */
+    ELE_BILL;
+}
---
+++ dcloud-common/src/main/java/net/xdclass/enums/ProductOrderStateEnum.java
@@ -0,0 +1,24 @@
+package net.xdclass.enums;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2023/11/30 00:28
+ */
+public enum ProductOrderStateEnum {
+
+    /**
+     * 未支付
+     */
+    NEW,
+
+    /**
+     * 已经支付
+     */
+    PAY,
+
+    /**
+     * 超时取消
+     */
+    CANCEL;
+}
