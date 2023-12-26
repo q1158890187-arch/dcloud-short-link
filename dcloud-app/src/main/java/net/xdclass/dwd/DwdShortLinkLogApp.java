5297de75186b5b20e209b810decf09a7e6e14e93
zhengqinghua <zhengqinghuaqaq@gmail.com>
2023-12-27 01:05:33 +0800

    feat: Kafka工具类封装

--- dcloud-app/src/main/java/net/xdclass/dwd/DwdShortLinkLogApp.java
+++ dcloud-app/src/main/java/net/xdclass/dwd/DwdShortLinkLogApp.java
@@ -1,8 +1,10 @@
 package net.xdclass.dwd;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import net.xdclass.util.KafkaUtil;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 /**
  * @description:
@@ -12,10 +14,22 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 @Slf4j
 public class DwdShortLinkLogApp {
 
+    /**
+     * 定义topic
+     */
+    public static final String SOURCE_TOPIC = "ods_link_visit_topic";
+
+    /**
+     * 定义消费者组
+     */
+    public static final String GROUP_ID = "dwd_short_link_group";
+
     public static void main(String [] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        DataStream<String> ds =  env.socketTextStream("127.0.0.1",8888);
+        // DataStream<String> ds =  env.socketTextStream("127.0.0.1",8888);
+        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(SOURCE_TOPIC, GROUP_ID);
+        DataStreamSource<String> ds = env.addSource(kafkaConsumer);
         ds.print();
         env.execute();
     }
