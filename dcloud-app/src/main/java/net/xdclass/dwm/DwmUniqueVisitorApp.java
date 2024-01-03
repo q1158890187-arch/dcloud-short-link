78aa02fb0af39f6e84b7e900abe924b7ed2e8d67
zhengqinghua <zhengqinghuaqaq@gmail.com>
2024-01-03 23:09:14 +0800

    feat:DWM层数据处理-短链UV统计开发

---
+++ dcloud-app/src/main/java/net/xdclass/dwm/DwmUniqueVisitorApp.java
@@ -0,0 +1,66 @@
+package net.xdclass.dwm;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import net.xdclass.func.UniqueVisitorFilterFunction;
+import net.xdclass.util.KafkaUtil;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2024/1/3 22:43
+ */
+public class DwmUniqueVisitorApp {
+    /**
+     * 定义source topic
+     */
+    public static final String SOURCE_TOPIC = "dwd_link_visit_topic";
+
+    /**
+     * 定义消费者组
+     */
+    public static final String GROUP_ID = "dwm_unique_visitor_group";
+
+
+    /**
+     * 定义输出
+     */
+    public static final String SINK_TOPIC = "dwm_unique_visitor_topic";
+
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        // DataStream<String> ds =  env.socketTextStream("127.0.0.1",8888);
+
+        // 1、获取流
+        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(SOURCE_TOPIC, GROUP_ID);
+        DataStreamSource<String> ds = env.addSource(kafkaConsumer);
+        //2、数据转换
+        SingleOutputStreamOperator<JSONObject> jsonDS = ds.map(jsonStr -> JSON.parseObject(jsonStr));
+        //3、分组
+        KeyedStream<JSONObject, String> keyedStream = jsonDS.keyBy(new KeySelector<JSONObject, String>() {
+            @Override
+            public String getKey(JSONObject value) throws Exception {
+                return value.getString("udid");
+            }
+        });
+        //4、排重过滤
+        SingleOutputStreamOperator<JSONObject> filterDS = keyedStream.filter(new UniqueVisitorFilterFunction());
+        filterDS.print("独立访客");
+        //5、转成字符串写入kafka
+        SingleOutputStreamOperator<String> uniqueVisitorDS = filterDS.map(obj -> obj.toJSONString());
+        FlinkKafkaProducer<String> kafkaProducer = KafkaUtil.getKafkaProducer(SINK_TOPIC);
+        //6、将sink写到dwm层，kafka存储
+        uniqueVisitorDS.addSink(kafkaProducer);
+        env.execute();
+    }
+
+}
---
+++ dcloud-app/src/main/java/net/xdclass/func/UniqueVisitorFilterFunction.java
@@ -0,0 +1,59 @@
+package net.xdclass.func;
+
+import com.alibaba.fastjson.JSONObject;
+import net.xdclass.util.TimeUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.RichFilterFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.configuration.Configuration;
+
+/**
+ * @description:
+ * @author: zhengqinghua
+ * @date: 2024/1/3 22:45
+ */
+public class UniqueVisitorFilterFunction extends RichFilterFunction<JSONObject> {
+
+
+    private ValueState<String> lastVisitDateState = null;
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        ValueStateDescriptor<String> visitDateStateDes = new ValueStateDescriptor<>("visitDateState", String.class);
+        //统计UV
+        StateTtlConfig stateTtlConfig = StateTtlConfig.newBuilder(Time.days(1)).build();
+        visitDateStateDes.enableTimeToLive(stateTtlConfig);
+        this.lastVisitDateState = getRuntimeContext().getState(visitDateStateDes);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+    }
+
+    @Override
+    public boolean filter(JSONObject jsonObj) throws Exception {
+        //获取当前访问时间
+        Long visitTime = jsonObj.getLong("ts");
+        String udid = jsonObj.getString("udid");
+        //当前访问时间
+        String currentVisitDate = TimeUtil.format(visitTime);
+        //获取上次的状态访问时间
+        String lastVisitDate = lastVisitDateState.value();
+        //用当前页面的访问时间和状态时间进行对比
+        if(StringUtils.isNotBlank(lastVisitDate) && currentVisitDate.equalsIgnoreCase(lastVisitDate)){
+            System.out.println(udid+" 已经在 "+currentVisitDate+"时间访问过");
+            return false;
+        }else {
+            System.out.println(udid+" 在 "+currentVisitDate+"时间初次访问");
+            lastVisitDateState.update(currentVisitDate);
+            return true;
+        }
+    }
+
+}
+
